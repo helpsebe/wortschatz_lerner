f82fafa84315d8e3eec1656cb88092084c210f86
danil@symfio.de <danil@symfio.de>
2023-04-23 21:11:58 +0200

    refactor: unnecessary imports removed

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Navbar, Nav, Container } from "react-bootstrap";
+import { Navbar, Nav } from "react-bootstrap";
 import { NavLink, useLocation } from "react-router-dom";
 import LogoIcon from "../../assets/LogoIcon";
 import { NavbarLogo } from "./style";
